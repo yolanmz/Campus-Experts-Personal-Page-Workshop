4c5db834f1a4d7bee260b31d7c43c6cf7e44077f
Tyrone Wong <tyronewong96@gmail.com>
2019-02-02 11:01:41 -0500

    Workshop template

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+function showContactForm(){
+    var view = document.getElementById("contact-form").getAttribute('display'); //mistake? getComputedStyle
+    //var view = getComputedStyle(document.getElementById("contact-form")).display;
+    document.getElementById("contact-form").style.display = (view === "none" ? "block": "none");
+
+}
+
+
+function setSubject(value){
+    console.log("success");
+    // document.getElementById("email-subject").setAttribute("value",value);
+}
